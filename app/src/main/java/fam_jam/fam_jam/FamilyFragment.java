6f8ad32a06bc59458453eb43ac9170bbbd57c7f2
gracewgao <ggaoww@gmail.com>
2020-07-11 18:14:39 -0400

    family inflater is workinG!

--- app/src/main/java/fam_jam/fam_jam/FamilyFragment.java
+++ app/src/main/java/fam_jam/fam_jam/FamilyFragment.java
@@ -20,13 +20,14 @@ import com.google.firebase.database.ValueEventListener;
 import java.util.EventListener;
 
 import fam_jam.fam_jam.model.Family;
+import fam_jam.fam_jam.model.Member;
 
 import static fam_jam.fam_jam.LoginActivity.famId;
 import static fam_jam.fam_jam.MainActivity.fireRef;
 
 public class FamilyFragment extends Fragment {
 
-    private TextView famNameTv, passcodeTv;
+    private TextView famNameTv, passcodeTv, countTv;
     LayoutInflater layoutInflater;
     LinearLayout fam_members;
 
@@ -37,6 +38,7 @@ public class FamilyFragment extends Fragment {
 
         famNameTv = v.findViewById(R.id.family_name_tv);
         passcodeTv = v.findViewById(R.id.passcode_tv);
+        countTv = v.findViewById(R.id.count_tv);
 
         fireRef.child("families").child(famId).addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
@@ -59,10 +61,39 @@ public class FamilyFragment extends Fragment {
         super.onViewCreated(view, savedInstanceState);
 
         fam_members = getView().findViewById(R.id.fam_members);
-        layoutInflater = (LayoutInflater)getActivity().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-        for (int i = 0; i < 5; i++) {
-            View myview = layoutInflater.inflate(R.layout.familymembercard, null, false);
-            fam_members.addView(myview);
-        }
+
+        fireRef.child("families").child(famId).child("members").addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                fam_members.removeAllViews();
+                countTv.setText(String.valueOf(dataSnapshot.getChildrenCount()));
+                for (DataSnapshot s : dataSnapshot.getChildren()){
+                    String m = (String) s.getValue();
+                    fireRef.child("members").child(m).addListenerForSingleValueEvent(new ValueEventListener() {
+                          @Override
+                          public void onDataChange(@NonNull DataSnapshot snapshot) {
+                              Member m = snapshot.getValue(Member.class);
+                              // creates view for each member
+                              layoutInflater = (LayoutInflater)getActivity().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+                              View myview = layoutInflater.inflate(R.layout.familymembercard, null, false);
+                              TextView name = myview.findViewById(R.id.name_tv);
+                              TextView pts = myview.findViewById(R.id.points_tv);
+                              name.setText(m.getName());
+                              pts.setText(String.valueOf(m.getPoints()));
+                              fam_members.addView(myview);
+                          }
+                          @Override
+                          public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                          }
+                      }
+                    );
+                }
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+            }
+        });
     }
+
 }
