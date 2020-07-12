376ae33e69ed155f134db06ad904c2938689c744
Bonnie Chin <bonniechin.mail@gmail.com>
2020-07-11 21:13:14 -0400

    wootwoot

--- app/src/main/java/fam_jam/fam_jam/LeaderboardFragment.java
+++ app/src/main/java/fam_jam/fam_jam/LeaderboardFragment.java
@@ -9,12 +9,20 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.ValueEventListener;
+
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import fam_jam.fam_jam.model.Member;
 
+import static fam_jam.fam_jam.LoginActivity.famId;
+import static fam_jam.fam_jam.MainActivity.fireRef;
+
 public class LeaderboardFragment extends Fragment {
+
+
 
     LayoutInflater layoutInflater;
     LinearLayout leaders;
@@ -29,12 +37,49 @@ public class LeaderboardFragment extends Fragment {
         super.onViewCreated(view, savedInstanceState);
 
         leaders = getView().findViewById(R.id.leaderboard);
-        layoutInflater = (LayoutInflater)getActivity().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+        fireRef.child("families").child(famId).child("members").addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                leaders.removeAllViews();
+                int i =0;
+                for (DataSnapshot s : dataSnapshot.getChildren()){
+
+                    String m = (String) s.getValue();
+                    fireRef.child("members").child(m).addListenerForSingleValueEvent(new ValueEventListener() {
+                                                                                         @Override
+                                                                                         public void onDataChange(@NonNull DataSnapshot snapshot) {
+                                                                                             Member m = snapshot.getValue(Member.class);
+                                                                                             // creates view for each member
+                                                                                             layoutInflater = (LayoutInflater)getActivity().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+                                                                                             View myview = layoutInflater.inflate(R.layout.leaderboardcard, null, false);
+                                                                                             TextView name = myview.findViewById(R.id.name_tv);
+                                                                                             name.setText(m.getName());
+                                                                                             leaders.addView(myview);
+                                                                                         }
+                                                                                         @Override
+                                                                                         public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                                                                                         }
+                                                                                     }
+                    );
+                    i++;
+                    if (i>=4){
+                        break;
+                    }
+                }
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+            }
+        });
+
+        /*layoutInflater = (LayoutInflater)getActivity().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
         for (int i = 0; i < 4; i++) {
             View myview = layoutInflater.inflate(R.layout.leaderboardcard, null, false);
             leaders.addView(myview);
 
 
-        }
+        }*/
     }
 }
--- app/src/main/java/fam_jam/fam_jam/ProfileFragment.java
+++ app/src/main/java/fam_jam/fam_jam/ProfileFragment.java
@@ -1,6 +1,10 @@
 package fam_jam.fam_jam;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.Drawable;
+import android.media.Image;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -10,10 +14,20 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+
+import okhttp3.Cache;
 
 public class ProfileFragment extends Fragment {
 
     private Button logoutButton;
+    public ImageView imageView;
 
     @Nullable
     @Override
@@ -29,6 +43,31 @@ public class ProfileFragment extends Fragment {
             }
         });
 
+
+
+        /*URL url = new URL("http://image10.bizrate-images.com/resize?sq=60&uid=2216744464");
+        Bitmap bmp = BitmapFactory.decodeStream(url.openConnection().getInputStream());
+
+        urlTest.setImageBitmap(bmp); */
+
+        /*InputStream is = null;
+        try {
+            is = (InputStream) new URL("https://external-preview.redd.it/KgNuAcsy0x59BmJu_dnEKaNPrP309fLkKYWxIOa0wBo.png?auto=webp&s=886ccb8afdf7abb69118293234ebcd995c3808e2").getContent();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        Drawable d = Drawable.createFromStream(is, "ihatelife");
+
+
+
+        urlTest.setBackground(d);*/
+        imageView = v.findViewById(R.id.urltest);
+        String potato = "https://external-preview.redd.it/KgNuAcsy0x59BmJu_dnEKaNPrP309fLkKYWxIOa0wBo.png?auto=webp&s=886ccb8afdf7abb69118293234ebcd995c3808e2";
+        Picasso.get().load(potato).into(imageView);
         return v;
+
+
     }
+
 }
+
